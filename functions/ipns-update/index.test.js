310b07fde0c8f5ac0bd04c57fa0092618dce781f
0xcharchar <87212793+0xcharchar@users.noreply.github.com>
2021-12-10 21:29:45 +0000

    test for when key is already set

--- functions/ipns-update/index.test.js
+++ functions/ipns-update/index.test.js
@@ -59,3 +59,26 @@ test('generate new key and update IPNS', async ({ equal }) => {
   equal(response.statusCode, 200)
   equal(response.body, JSON.stringify({ ipnsHash: 'k51qzi5uqu5dkdh3b3p49ah43mfah8u9t2ss4wv1epdtio0vrp9bd6h0sucs80' }))
 })
+
+test('use existing key and update IPNS', async ({ equal }) => {
+  const ipfsNode = nock('http://localhost:5001', {"encodedQueryParams":true})
+    // Key list
+    .post('/api/v0/key/list')
+    .reply(200, { "Keys": [
+      { Name: 'wee.me3.eth', Id: 'k51qzi5uqu5di0tro50duiuaur4htjfp7ufcp3g2yitbbohf7a3dyqd1ur8fm7' }
+    ] })
+    // Key generation
+    .post('/api/v0/key/gen')
+    .query({"type":"ed25519","arg":"wee.me3.eth"})
+    .reply(200, {"Name":"wee.me3.eth","Id":"k51qzi5uqu5di0tro50duiuaur4htjfp7ufcp3g2yitbbohf7a3dyqd1ur8fm7"})
+    // Publish to IPNS
+    .post('/api/v0/name/publish')
+    .query({"key":"wee.me3.eth","lifetime":"24h","arg":"/ipfs/QmTye6k2H42QAJYuz4yefMDeBQVWqPoKE52FHGAFudra2G"})
+    .reply(200, { Name: 'wee.me3.eth', Value: 'k51qzi5uqu5dkdh3b3p49ah43mfah8u9t2ss4wv1epdtio0vrp9bd6h0sucs80' })
+
+  const ev = makeEvent({ subdomain: 'wee.me3.eth', ipfsHash: 'QmTye6k2H42QAJYuz4yefMDeBQVWqPoKE52FHGAFudra2G' })
+  const response = await handler(ev)
+
+  equal(response.statusCode, 200)
+  equal(response.body, JSON.stringify({ ipnsHash: 'k51qzi5uqu5dkdh3b3p49ah43mfah8u9t2ss4wv1epdtio0vrp9bd6h0sucs80' }))
+})
